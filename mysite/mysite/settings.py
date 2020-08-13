aceedf5c030a89e57e38446f200bfed72fb6251e
manolis <manos.korniotakis@gmail.com>
2020-08-13 15:22:33 +0300

    An effort to migrate v2of app to Heroku.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -63,7 +63,6 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.github',
     'allauth.socialaccount.providers.google',
     'django_template_check',
     'bootstrap4',
